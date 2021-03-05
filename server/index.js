e7032fb546d131247ecd41ca6cd573f5dc43cf30
Admin <Admin@DESKTOP-5PBQ2D3>
2021-03-05 11:21:14 +0200

    update to the dynamic heroku por

---
+++ server/index.js
@@ -0,0 +1,14 @@
+//get the express library
+const express = require('express')
+//create express application
+const app = express();
+
+//create a route handler
+app.get('/', (req, res) =>{
+    res.send({hi: 'there'})
+});
+
+//define the port to listen from Heroku 
+const PORT = process.env.PORT || 5000
+//listen to the app at port 5000
+app.listen(PORT);
